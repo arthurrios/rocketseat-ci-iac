07a657dbd81d3b1b185312ad0a225b9a27f71036
arthur <arthur.rios007@gmail.com>
2025-03-07 22:34:27 -0300

    new: first commit

---
+++ ecr.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "rocketseat-ci-api" {
+  name                 = "rocketseat-ci"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+
+  tags = {
+    IAC = true
+  }
+}
---
+++ iam.tf
@@ -0,0 +1,103 @@
+resource "aws_iam_openid_connect_provider" "oidc-git" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  tags = {
+    IAC = true
+  }
+}
+
+resource "aws_iam_role_policy" "my_policy" {
+  name        = "ecr-app-permission"
+  role        = aws_iam_role.ecr_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid = "Statement1",
+      Action = "apprunner:*",
+      Effect = "Allow",
+      Resource = "*"
+    },
+    {
+      Sid = "Statement2",
+      Action = [
+        "iam:PassRole",
+        "iam:CreateServiceLinkedRole",
+      ],
+      Effect = "Allow",
+      Resource = "*"
+    },
+    {
+        Sid = "Statement3",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "app-runner-role-ecr" {
+  role       = aws_iam_role.app-runner-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          Service = "build.apprunner.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    IAC = true
+  } 
+}
+
+resource "aws_iam_role" "ecr_role" {
+  name = "ecr_role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+        Statement = [{
+        "Principal": {
+            "Federated": "arn:aws:iam::266735825067:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+              "token.actions.githubusercontent.com:sub": "repo:arthurrios/rocketseat-ci-api:ref:refs/heads/main",
+              "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          }
+        }
+        Effect = "Allow"
+      },
+    ]
+  })
+
+  tags = {
+    IAC = true
+  }
+}
